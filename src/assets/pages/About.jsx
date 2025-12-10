9459a218bc36fe9c60c4abfea6e4c1073c42654c
balibali <bilalnaqvi592@gmail.com>
2025-12-10 11:34:54 +0500

    products remove, about page

--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -4,6 +4,20 @@ function About() {
   return (
     <>
       <h2 className="container">About page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque, ad
+        asperiores eius veritatis fuga nobis ut aut ex quod fugiat at officiis
+        ab? Voluptas tenetur modi ullam dignissimos corrupti molestias excepturi
+        nulla dolorum, consequuntur rem. Repellendus quasi, pariatur nesciunt
+        necessitatibus dolores, sequi exercitationem, architecto culpa sit ad
+        quam consequatur quis voluptate nisi ullam. Voluptates repudiandae
+        distinctio quia asperiores. Cupiditate mollitia officia modi
+        necessitatibus aut quasi velit magnam similique ad labore laboriosam
+        tempora, itaque corporis, nemo ipsum blanditiis eum ducimus. Dolorem
+        cumque doloribus commodi inventore impedit quidem quaerat explicabo
+        expedita at vero dolorum, dolores enim, eum facere illo earum laborum.
+      </p>
+      <button>Click me</button>
     </>
   );
 }
